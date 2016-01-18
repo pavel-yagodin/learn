0535f7eb46f76ddaf1b83f86aef7b57b749a3aed
pavel-yagodin <pavel.yagodin@gmail.com>
2016-01-18 05:42:20 +0200

    deploy

--- app/scripts/app.js
+++
@@ -1,6 +0,0 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-import Example from '../blocks/example';
-
-$(() => svg4everybody());
-$(() => new Example().render());
--- gulp/deploy.js
+++
@@ -1,7 +0,0 @@
-import gulp from 'gulp';
-import runSequence from 'run-sequence';
-import ghpages from 'gulp-gh-pages';
-
-gulp.task('deploy', () => (
-	gulp.src(['dist/**/*', '!dist/robots.txt']).pipe(ghpages({branch: 'gh-pages'}))
-));
